5a2f91ca1eb5f715ab4bc2654a6a34777fb89e15
KenDickinson <ken.dickinson20@gmail.com>
2017-10-11 22:59:20 -0400

    second commit

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  providers: [HeroService]
+  styleUrls: [ './heroes.component.css' ]
 })
-
-export class HeroesComponent implements OnInit { 
+export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
-  
+
   constructor(
-    private heroService: HeroService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -34,4 +32,17 @@ export class HeroesComponent implements OnInit {
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+    }
+
 }
